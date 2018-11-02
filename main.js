0f784ab2be99b728105badda885144a5a4c7839f
Yichuan Shen <shen.yichuan@gmail.com>
2018-11-02 14:29:54 +0100

    Adopt new Shudan styling strategy

--- main.js
+++ main.js
@@ -9,44 +9,49 @@ async function main() {
         (file, stats) => !stats.isDirectory() && path.extname(file) !== '.css'
     ])
 
-    await Promise.all(
-        cssFiles.map(file => (async () => {
-            console.log(`Processing ${path.relative(folderPath, file)}...`)
+    await Promise.all(cssFiles.map(file => (async () => {
+        console.log(`Processing ${path.relative(folderPath, file)}...`)
 
-            let css = await promisify(fs.readFile)(file, 'utf8')
+        let css = await promisify(fs.readFile)(file, 'utf8')
 
-            let newCSS = css
-                .replace(
-                    /\.goban\s+(\.row\s+)?li(\.random_(\d+))?\.sign_(-1|0|1)(\.random_(\d+))?\s+\.stone\s+img/g,
-                    (_0, _1, _2, random1, sign, _3, random2) => {
-                        let random = +(random1 || random2 || 0)
-                        let randomClass = random !== 0 ? `.shudan-random_${random}` : ''
+        let newCSS = css
+            .replace(
+                /\.goban\s+(\.row\s+)?li(\.random_(\d+))?\.sign_(-1|0|1)(\.random_(\d+))?\s+\.stone\s+img/g,
+                (_0, _1, _2, random1, sign, _3, random2) => {
+                    let random = +(random1 || random2 || 0)
+                    let randomClass = random !== 0 ? `.shudan-random_${random}` : ''
 
-                        return `.shudan-vertex.shudan-sign_${sign}${randomClass} .shudan-inner`
-                    }
-                )
-                .replace(/\.goban\s+(\.row\s+)?li/g, '.shudan-vertex')
-                .replace(
-                    /(([^,{]+,\s*)*)\.goban((\s*,[^,{]+)*)\s*{([^{}])}/g,
-                    (_0, before, _1, after, _2, body) => {
-                        let colorRegex = /(#[0-9a-f]{6}|rgba?\((\s*[\d\.]+\s*,?){3,4}\))/i
-                        let boardBackgroundColor = (body.match(colorRegex) || [])[0]
+                    return `.shudan-stone-image.shudan-sign_${sign}${randomClass}`
+                }
+            )
+            .replace(
+                /(([^,{]+,\s*)*)\.goban((\s*,[^,{]+)*)\s*{([^{}])}/g,
+                (_0, _1, _2, _3, _4, body) => {
+                    let urlRegex = /background-image:\s+(url\([^)]*\))/
+                    let boardBackgroundImage = (body.match(urlRegex) || [])[1]
+                    let colorRegex = /(#[0-9a-f]{6}|rgba?\((\s*[\d\.]+\s*,?){3,4}\))/i
+                    let boardBackgroundColor = (body.match(colorRegex) || [])[0]
 
-                        return [
-                            `${before}.shudan-goban${after} {`,
-                            '  ' + body.trim(),
-                            boardBackgroundColor && `  --shudan-board-background-color: ${boardBackgroundColor};`,
+                    return [
+                        boardBackgroundColor && [
+                            `.shudan-goban {`,
+                            `  --shudan-board-background-color: ${boardBackgroundColor};`,
                             '}'
-                        ].filter(x => x).join('\n')
-                    }
-                )
-                .replace(/\.goban/g, '.shudan-goban')
+                        ].join('\n'),
+                        '',
+                        boardBackgroundImage && [
+                            '.shudan-goban-image {',
+                            `  background-image: ${boardBackgroundImage}`,
+                            '}'
+                        ].join('\n')
+                    ].join('\n')
+                }
+            )
 
-            await promisify(fs.writeFile)(file, newCSS)
+        await promisify(fs.writeFile)(file, newCSS)
 
-            console.log(`Done processing ${path.relative(folderPath, file)}.`)
-        })())
-    )
+        console.log(`Done processing ${path.relative(folderPath, file)}.`)
+    })()))
 
     console.log('Done.')
 }
